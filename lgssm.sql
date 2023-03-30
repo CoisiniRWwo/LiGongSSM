65fa54d536e2def1326c0b645562aff3862d6952
CoisiniRWwo <1328234978@qq.com>
2023-03-30 20:49:10 +0800

    sql

--- lgssm.sql
+++ lgssm.sql
@@ -11,11 +11,27 @@
  Target Server Version : 80029 (8.0.29)
  File Encoding         : 65001
 
- Date: 20/03/2023 15:49:03
+ Date: 30/03/2023 20:47:13
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `id` int NOT NULL,
+  `typename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES (1, '黑色家电');
+INSERT INTO `category` VALUES (2, '白色家电');
 
 -- ----------------------------
 -- Table structure for dm_student
@@ -23,23 +39,22 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `dm_student`;
 CREATE TABLE `dm_student`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `sno` int NULL DEFAULT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `major` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `sno` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of dm_student
 -- ----------------------------
-INSERT INTO `dm_student` VALUES (1, '小明', 'a', 1);
-INSERT INTO `dm_student` VALUES (2, '小红', 'b', 1);
-INSERT INTO `dm_student` VALUES (3, '小白', 'c', 2);
-INSERT INTO `dm_student` VALUES (4, '小新', 'a', 2);
-INSERT INTO `dm_student` VALUES (5, '小黑', 'c', 1);
-INSERT INTO `dm_student` VALUES (6, '小王', 'b', NULL);
-INSERT INTO `dm_student` VALUES (9, '小小', 'a', 3);
-INSERT INTO `dm_student` VALUES (10, '中中', 'a', 2);
+INSERT INTO `dm_student` VALUES (1, '张三', '数学', '10001');
+INSERT INTO `dm_student` VALUES (2, '李四', '英语', '10002');
+INSERT INTO `dm_student` VALUES (3, '王五', '计算机', '10003');
+INSERT INTO `dm_student` VALUES (4, '王刚', '化学', '10004');
+INSERT INTO `dm_student` VALUES (5, '李华', '物理', '10005');
+INSERT INTO `dm_student` VALUES (6, '李雷', '中文', '10006');
+INSERT INTO `dm_student` VALUES (7, '关羽', '俄语', '10007');
 
 -- ----------------------------
 -- Table structure for employee
@@ -60,6 +75,161 @@ INSERT INTO `employee` VALUES (1, '张三', 20, '员工');
 INSERT INTO `employee` VALUES (2, '李四', 18, '员工');
 INSERT INTO `employee` VALUES (3, '王五', 35, '经理');
 INSERT INTO `employee` VALUES (12, '赵六', 20, '老板');
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product`  (
+  `id` int NOT NULL,
+  `goodsname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `price` int NULL DEFAULT NULL,
+  `typeid` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+INSERT INTO `product` VALUES (1, '电视机', 5000, 1);
+INSERT INTO `product` VALUES (2, '冰箱', 4000, 2);
+INSERT INTO `product` VALUES (3, '空调', 3000, 2);
+INSERT INTO `product` VALUES (4, '洗衣机', 2000, 2);
+
+-- ----------------------------
+-- Table structure for tb_book
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_book`;
+CREATE TABLE `tb_book`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bookName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `price` double NULL DEFAULT NULL,
+  `author` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_book
+-- ----------------------------
+INSERT INTO `tb_book` VALUES (1, 'Java基础入门', 45, '	传智播客高教产品研发部');
+INSERT INTO `tb_book` VALUES (2, 'Java Web程序开发进阶', 45, '黑马程序员');
+INSERT INTO `tb_book` VALUES (3, 'MySQL数据库入门', 40, '黑马程序员');
+
+-- ----------------------------
+-- Table structure for tb_idcard
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_idcard`;
+CREATE TABLE `tb_idcard`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `CODE` varchar(18) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_idcard
+-- ----------------------------
+INSERT INTO `tb_idcard` VALUES (1, '152221198711020624');
+INSERT INTO `tb_idcard` VALUES (2, '152201199008150317');
+
+-- ----------------------------
+-- Table structure for tb_orders
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_orders`;
+CREATE TABLE `tb_orders`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `number` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `tb_orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_orders
+-- ----------------------------
+INSERT INTO `tb_orders` VALUES (1, '1000011', 1);
+INSERT INTO `tb_orders` VALUES (2, '1000012', 1);
+INSERT INTO `tb_orders` VALUES (3, '1000013', 2);
+
+-- ----------------------------
+-- Table structure for tb_ordersitem
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_ordersitem`;
+CREATE TABLE `tb_ordersitem`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `orders_id` int NULL DEFAULT NULL,
+  `product_id` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `orders_id`(`orders_id` ASC) USING BTREE,
+  INDEX `product_id`(`product_id` ASC) USING BTREE,
+  CONSTRAINT `tb_ordersitem_ibfk_1` FOREIGN KEY (`orders_id`) REFERENCES `tb_orders` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `tb_ordersitem_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `tb_product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_ordersitem
+-- ----------------------------
+INSERT INTO `tb_ordersitem` VALUES (1, 1, 1);
+INSERT INTO `tb_ordersitem` VALUES (2, 1, 3);
+INSERT INTO `tb_ordersitem` VALUES (3, 2, 2);
+INSERT INTO `tb_ordersitem` VALUES (4, 3, 3);
+
+-- ----------------------------
+-- Table structure for tb_person
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_person`;
+CREATE TABLE `tb_person`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `age` int NULL DEFAULT NULL,
+  `sex` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `card_id` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `card_id`(`card_id` ASC) USING BTREE,
+  CONSTRAINT `tb_person_ibfk_1` FOREIGN KEY (`card_id`) REFERENCES `tb_idcard` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_person
+-- ----------------------------
+INSERT INTO `tb_person` VALUES (1, 'Rose', 22, '女', 1);
+INSERT INTO `tb_person` VALUES (2, 'jack', 23, '男', 2);
+
+-- ----------------------------
+-- Table structure for tb_product
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_product`;
+CREATE TABLE `tb_product`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `price` double NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_product
+-- ----------------------------
+INSERT INTO `tb_product` VALUES (1, 'Java基础入门', 44.5);
+INSERT INTO `tb_product` VALUES (2, 'Java Web程序开发入门', 38.5);
+INSERT INTO `tb_product` VALUES (3, 'SSM框架整合实战', 50);
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `address` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
+INSERT INTO `tb_user` VALUES (1, '小明', '北京');
+INSERT INTO `tb_user` VALUES (2, '李华', '上海');
+INSERT INTO `tb_user` VALUES (3, '李刚', '上海');
 
 -- ----------------------------
 -- Table structure for users
